16d248bf01d0271be60989a34e46b306d53ff85c
Hector Gomez <hector.gomez@teaminternational.com>
2020-08-02 19:44:47 -0500

    Remove unused code

--- src/components/results/page.js
+++ src/components/results/page.js
@@ -1,5 +1,4 @@
- import React, { Fragment, useEffect, useState } from 'react';
-// import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,78 +13,46 @@ function Page(props) {
         results,
         goTo,
         text,
-    } = props;       
-    console.log(results);
-    
-    const [result, setResult] = React.useState([]);
-    const [poke, setPoke] = React.useState([]);
-    const arr = [];     
+    } = props;
 
-    useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
-            .then((response) => response.json())
-            .then((data) => setResult(
-              data.results.map((item) => {
-                fetch(item.url)
-                  .then((response) => response.json())
-                  .then((allpokemon) => arr.push(allpokemon));
-                  setPoke(arr);
-              }),
-            ));
-        }, []); 
-
-    // const [count, setCount] = useState(0);
-    
-
-    // // Similar to componentDidMount and componentDidUpdate:
-    // useEffect(() => {
-    //     // Update the document title using the browser API
-    //     document.title = `You clicked ${count} times`;
-    //     isEmpty = false;
-    // });
-
-    //  console.log(poke);
-
-    if(text != null){        
+    if (text != null) {
         goTo(`/details/${text}`);
     }
 
     return (
         <Fragment>
             <CssBaseline />
-            <AppBar />
-
-            {/* <div onClick={() => setCount(count + 1)} className="results-page"> */}
-            <div  className="results-page">
-                {results.map(item =>
-                    <div
-                        key={item.id}
-                        className="card-container"
-                    >
-                        <Card
-                            className="card"
-                            onClick={() => goTo(`/details/${item.id}`)} >
-                            <CardActionArea>                                    
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2" align="center">
-                                        {item.name.toUpperCase()}
-                                    </Typography>
-                                    <CardMedia
-                                        className="card-media"
-                                        image={item.sprites.front_default}
-                                        title={item.name}
-                                    />
-                                    <Typography component="p" align="center">
-                                        {item.types.map(element => 
-                                            element.type.name.toUpperCase() + ' '
-                                        )}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
+            <div onClick={() => goTo(`/results/`)} >
+                <AppBar />
+            </div>
+            {results.map(item =>
+                <div
+                    key={item.id}
+                    className="card-container"
+                >
+                    <Card
+                        className="card"
+                        onClick={() => goTo(`/details/${item.id}`)} >
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2" align="center">
+                                    {item.name.toUpperCase()}
+                                </Typography>
+                                <CardMedia
+                                    className="card-media"
+                                    image={item.sprites.front_default}
+                                    title={item.name}
+                                />
+                                <Typography component="p" align="center">
+                                    {item.types.map(element =>
+                                        element.type.name.toUpperCase() + ' '
+                                    )}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 </div>)
             }
-            </div>
         </Fragment>
     );
 }
